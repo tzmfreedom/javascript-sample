bd611d8361215b68a68e5d2fd0118c9902f8b7ea
tzmfreedom <makoto_tajitsu@hotmail.co.jp>
2019-12-09 18:07:56 +0900

    add calendar ui

--- components/dialog/app.js
+++ components/dialog/app.js
@@ -9,6 +9,8 @@ window.onclick = (event) => {
     modal.style.display = 'none';
   } else if (event.target === modal2) {
     modal2.style.display = 'none';
+  } else {
+    // console.log(event);
   }
 };
 
@@ -49,6 +51,23 @@ function dragElement(elm) {
 const calendarBtn = document.getElementById('calendar-button');
 const inputDate = document.getElementById('input-date')
 const calendarUi = document.getElementById('calendar-ui');
+const calendarUiBody = document.getElementById('calendar-ui-body');
+const calendarPrevious = document.getElementById('calendar-previous');
+const calendarNext = document.getElementById('calendar-next');
+const calendarTitle = document.getElementById('calendar-title');
+
+let currentYear = (new Date()).getFullYear();
+let currentMonth = (new Date()).getMonth();
+
+inputDate.onfocus = (e) => {
+  calendarUi.style.left = (inputDate.offsetLeft) + 'px';
+  calendarUi.style.top = (inputDate.offsetTop + inputDate.offsetHeight) + 'px';
+  calendarUi.style.display = 'block';
+};
+
+inputDate.onblur = (e) => {
+  calendarUi.style.display = 'none';
+};
 
 calendarBtn.onclick = (e) => {
   calendarUi.style.left = (inputDate.offsetLeft) + 'px';
@@ -56,11 +75,57 @@ calendarBtn.onclick = (e) => {
   calendarUi.style.display = 'block';
 };
 
-const elems = document.getElementsByClassName('day');
-for (let i = 0; i < elems.length; i++) {
-  const elm = elems[i];
-  elm.onclick = () => {
-    inputDate.value = "2017-11-" + elm.textContent.padStart(2, '0');
-    calendarUi.style.display = 'none';
-  };
+calendarPrevious.onclick = (e) => {
+  if (currentMonth === 0) {
+    currentYear--;
+    currentMonth = 11;
+  } else {
+    currentMonth--;
+  }
+  showCalendar(currentYear, currentMonth);
+};
+
+calendarNext.onclick = (e) => {
+  if (currentMonth === 11) {
+    currentYear++;
+    currentMonth = 0;
+  } else {
+    currentMonth++;
+  }
+  showCalendar(currentYear, currentMonth);
+};
+
+const daysInMonth = (year, month) => {
+  return 32 - new Date(year, month, 32).getDate();
+};
+
+const showCalendar = (year, month) => {
+  calendarTitle.textContent = `${year}-${(month+1).toString().padStart(2, '0')}`;
+
+  const firstDay = (new Date(year, month)).getDay();
+  let row = document.createElement('tr');
+  const days = daysInMonth(year, month);
+  for (let i = 0; i < firstDay; i++) {
+    const col = document.createElement('td');
+    row.append(col);
+  }
+  calendarUiBody.textContent = '';
+  for (let i = 0; i < days; i++) {
+    const col = document.createElement('td');
+    col.className = 'day';
+    col.textContent = (i+1).toString();
+    col.onmousedown = () => {
+      // debugger;
+      inputDate.value = `${year}-${(month+1).toString().padStart(2, '0')}-${col.textContent.padStart(2, '0')}`;
+      calendarUi.style.display = 'none';
+    };
+    row.append(col);
+    if ((i + firstDay) % 7 === 6) {
+      calendarUiBody.append(row);
+      row = document.createElement('tr');
+    }
+  }
+  calendarUiBody.append(row);
 }
+
+showCalendar(currentYear, currentMonth);
